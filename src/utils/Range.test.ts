3db9269f1d57c617fe495471897c986fa4443036
Yuji Suzuki <y0suzuki0123@gmail.com>
2019-04-29 14:57:10 +0900

    Refine test of updateRange

--- src/utils/Range.test.ts
+++ src/utils/Range.test.ts
@@ -2,13 +2,14 @@ import { updateAt, updateRange } from './Range';
 
 describe('updateRange', () => {
   test('update range', () => {
-    const input = ['one', 'two', 'three'];
-    const range = { from: 1, to: 2 };
+    const input = ['one', 'two', 'three', 'four'];
+    const range = { from: 1, to: 3 };
 
     expect(updateRange(input, range, v => v.toUpperCase())).toEqual([
       'one',
       'TWO',
-      'three',
+      'THREE',
+      'four',
     ]);
   });
 });
